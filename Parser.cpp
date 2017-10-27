23cb04b8994f64c4ac0e21af143ac41c7e924f0f
Devon Graves <dhg5054@gmail.com>
2017-10-27 08:25:19 -0400

    Add files via upload

---
+++ Parser.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
+
+struct rule
+{
+	vector<string> item;
+	string leadTerminal;
+} rules[5];
+
+struct state
+{
+	vector<string> items;
+} states[9];
+
+void readGrammar(ifstream &inputFile);
+void createStateDiagram();
+void stateClosure(struct state);
+void addTerminal(string newTerminal);
+
+vector<string> terminals;
+vector<state> stateDiagram;
+vector<tuple<int, int>> edges;
+
+int main()
+{
+	ifstream inputFile;
+	inputFile.open("input.txt");
+
+	// File is not found
+	if (!inputFile.is_open())
+	{
+		cout << "ERROR! NO FILE FOUND!\n";
+		system("PAUSE");
+		return 1;
+	}
+
+	// File is found
+	else
+	{
+		cout << "File opened successfully!\n";
+	}
+
+	readGrammar(inputFile);
+	createStateDiagram();
+	cout << terminals.at(0);
+	return 0;
+}
+
+void readGrammar(ifstream &inputFile)
+{
+	char in;
+	int ruleNum = 0;
+	string newItem;
+	while (!inputFile.eof())
+	{
+		
+		while (inputFile.peek() != '\n' && !inputFile.eof())
+		{
+			inputFile.get(in);
+			if (!isspace(in))
+			{
+				if (in == '-') // Sets the leadTerminal equal to the value of the current rule (only the terminal value).
+				{
+					rules[ruleNum].leadTerminal = newItem;
+					addTerminal(newItem);
+				}
+
+				newItem += in; // Adds character to the new item.
+
+				if (in == '>') // Adds a dot after the arrow in the rule.
+				{
+					newItem += '.';
+				}
+			}
+		}
+		rules[ruleNum].item.push_back(newItem);
+		ruleNum++;
+		inputFile.get(in);
+		newItem = "";
+	}
+}
+
+// Adds a new terminal to the list of terminals.
+void addTerminal(string newTerminal)
+{
+	if (find(terminals.begin(), terminals.end(), newTerminal) == terminals.end())
+	{
+		terminals.push_back(newTerminal);
+	}
+}
+
+// Start of the creation of the state diagram. Starts by passing the first grammar rule into stateClosure.
+void createStateDiagram()
+{
+	states[0].items.push_back(rules[0].item.at(0));
+	stateClosure(states[0]);
+}
+
+// Adds all relevant rules to the current state that was passed in.
+void stateClosure(struct state currentState)
+{
+	int pos;
+	for (int i = 0; i < currentState.items.size(); i++) // Iterates through all items in a state
+	{
+		pos = currentState.items.at(i).find('.');
+		for (int k = 0; k < terminals.size(); k++) // Iterates through all terminals in a state
+		{
+			if (pos != currentState.items.at(i).size() - 1 && terminals.at(k).compare(currentState.items.at(pos + 1)))
+			{
+
+			}
+		}
+	}
+}
+
+
+void findGoTo() {} // This will add items to a state. The last part of this function should be to call stateClosure to close out the state.
+
+void reduceStack() {} // This will essentially pop the states backwards. Might have to use a stack to track what state we are creating.
