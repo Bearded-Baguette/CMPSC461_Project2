49251b70056240f0bc81a9fe5b0bb1e6c90bb104
Devon Graves <dhg5054@gmail.com>
2017-10-28 03:21:41 -0400

    Add files via upload

--- Parser.cpp
+++ Parser.cpp
@@ -10,40 +10,28 @@ using namespace std;
 
 struct rule
 {
-	vector<string> item;
-	string leadTerminal;
+	string item;
+	string startTerminal;
 } rules[5];
 
 struct state
 {
 	vector<string> items;
-} states[9];
+};
 
 void readGrammar(ifstream &inputFile);
 void createStateDiagram();
-void stateClosure(struct state);
-void addTerminal(string newTerminal);
-
-vector<string> terminals;
-vector<state> stateDiagram;
-vector<tuple<int, int>> edges;
-
-using namespace std;
+void stateClosure(struct state&);
+void addNonTerminal(string newTerminal);
+bool isNonTerminal(string);
+bool ruleIsAlreadyInState(struct state, string);
+void findEdges(string, string, int);
+string advanceDot(string);
 
 vector<string> terminals;
 vector<string> nonTerminals;
-vector<rule> states;
 vector<tuple<int, int>> edges;
-
-struct rule
-{
-	vector<char> item;
-	string leadTerminal;
-} rules[5];
-
-
-void readGrammar(ifstream &inputFile);
-void createStateDiagram(vector<rule>& states);
+vector<state> states;
 
 int main()
 {
@@ -54,7 +42,6 @@ int main()
 	if (!inputFile.is_open())
 	{
 		cout << "ERROR! NO FILE FOUND!\n";
-		system("PAUSE");
 		return 1;
 	}
 
@@ -66,8 +53,15 @@ int main()
 
 	readGrammar(inputFile);
 	createStateDiagram();
-	cout << terminals.at(0);
-	cout << rules[0].leadTerminal;
+	for (int k = 0; k < states.size();k++)
+	{
+		for (int i = 0; i < states.at(k).items.size(); i++)
+		{
+			cout << "State " << i << endl;
+			cout << "-----------------\n";
+			cout << states.at(k).items.at(i) << endl;
+		}
+	}
 	cout << "Done";
 	return 0;
 }
@@ -79,70 +73,136 @@ void readGrammar(ifstream &inputFile)
 	string newItem;
 	while (!inputFile.eof())
 	{
-		
-	while (!inputFile.eof())
-	{
-		vector <char> newItem;
-		while (inputFile.peek() != '\n' && !inputFile.eof())
+
+		while (!inputFile.eof())
 		{
+			while (inputFile.peek() != '\n' && !inputFile.eof())
+			{
+				inputFile.get(in);
+				if (!isspace(in))
+				{
+					if (in == '-') // Sets the startTerminal equal to the value of the current rule (only the terminal value).
+					{
+						rules[ruleNum].startTerminal = newItem;
+						addNonTerminal(newItem);
+					}
+
+					newItem += in; // Adds character to the new item.
+
+					if (in == '>') // Adds a dot after the arrow in the rule.
+					{
+						newItem += '.';
+					}
+				}
+			}
+			rules[ruleNum].item = newItem;
+			ruleNum++;
 			inputFile.get(in);
-			if (!isspace(in))
-			{
-				if (in == '-') // Sets the leadTerminal equal to the value of the current rule (only the terminal value).
-				{
-					rules[ruleNum].leadTerminal = newItem;
-					addTerminal(newItem);
-				}
-
-				newItem += in; // Adds character to the new item.
-
-				if (in == '>') // Adds a dot after the arrow in the rule.
-				{
-					newItem += '.';
-				}
-			}
+			newItem = "";
 		}
-		rules[ruleNum].item.push_back(newItem);
-		ruleNum++;
-		inputFile.get(in);
-		newItem = "";
 	}
 }
 
-// Adds a new terminal to the list of terminals.
-void addTerminal(string newTerminal)
-{
-	if (find(terminals.begin(), terminals.end(), newTerminal) == terminals.end())
+	// Adds a new terminal to the list of terminals.
+	void addNonTerminal(string newNonTerminal)
 	{
-		terminals.push_back(newTerminal);
-	}
-}
-
-// Start of the creation of the state diagram. Starts by passing the first grammar rule into stateClosure.
-void createStateDiagram()
-{
-	states[0].items.push_back(rules[0].item.at(0));
-	stateClosure(states[0]);
-}
-
-// Adds all relevant rules to the current state that was passed in.
-void stateClosure(struct state currentState)
-{
-	int pos;
-	for (int i = 0; i < currentState.items.size(); i++) // Iterates through all items in a state
-	{
-		pos = currentState.items.at(i).find('.');
-		for (int k = 0; k < terminals.size(); k++) // Iterates through all terminals in a state
+		if (find(nonTerminals.begin(), nonTerminals.end(), newNonTerminal) == nonTerminals.end())
 		{
-			if (pos != currentState.items.at(i).size() - 1 && terminals.at(k).compare(currentState.items.at(pos + 1)))
-			{
+			nonTerminals.push_back(newNonTerminal);
+		}
+	}
 
+	// Start of the creation of the state diagram. Starts by passing the first grammar rule into stateClosure.
+	void createStateDiagram()
+	{
+		state newState{};
+		states.push_back(newState);
+		states[0].items.push_back(rules[0].item);
+		stateClosure(states[0]);
+		for (int i = 0; i < states.size(); i++)
+		{
+			for (int j = 0; j < states[i].items.size(); j++)
+			{
+				int pos = states[i].items.at(j).find('.');
+				if (pos != states[i].items.at(j).size() - 1)
+				{
+					string c(1, states[i].items.at(j).at(pos + 1));
+					findEdges(states[i].items.at(j), c, pos);
+				}
 			}
 		}
 	}
-}
 
+	// Adds all relevant rules to the current state that was passed in.
+	void stateClosure(struct state &currentState)
+	{
+		int pos;
+		for (int i = 0; i < currentState.items.size(); i++) // Iterates through all items in a state
+		{
+			pos = currentState.items.at(i).find('.'); // Find the location of the dot in the item
+			if (pos != (currentState.items.at(i).size() - 1)) // If the dot is not at the end of the item
+			{
+				string c(1, currentState.items.at(i).at(pos + 1)); // Converts character after the dot to a string
+				for (int k = 0; k < nonTerminals.size(); k++) // Iterate through the list of nonTerminals
+				{
+					if (isNonTerminal(c))
+					{
+						for (int j = 0; j < 5; j++) // Iterate through the different rules
+						{
+							if (rules[j].startTerminal == c && !ruleIsAlreadyInState(currentState, rules[j].item)) // If the starting nonTerminal matches the nonTerminal in the current state and the rule is not already in the state
+							{
+								currentState.items.push_back(rules[j].item);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
-void findGoTo() {} // This will add items to a state. The last part of this function should be to call stateClosure to close out the state.
+	// This will add items to a state. The last part of this function should be to call stateClosure to close out the state.
+	void findEdges(string item, string symbol, int dotPos)
+	{
+		if (symbol == "$"){return;}
+		if (item.size() - 1 == dotPos) { return; }
+		state newState;
+		states.push_back(newState); // Add new state to the list of states
+		states.at(states.size() - 1).items.push_back(advanceDot(item));
+		stateClosure(states.at(states.size() - 1));
+	}
 
-void reduceStack() {} // This will essentially pop the states backwards. Might have to use a stack to track what state we are creating.
+	void reduceStack() {} // This will essentially pop the states backwards. Might have to use a stack to track what state we are creating.
+
+	// Checks if a passed character is in the nonTerminal list. Should have passed in a string, but no terminals we're passing in will be more than 1 character long.
+	bool isNonTerminal(string c)
+	{
+		if (find(nonTerminals.begin(), nonTerminals.end(), c) != nonTerminals.end())
+		{
+			return true;
+		}
+		return false;
+	}
+
+	// Checks to see if a new rule being added is already in the state.
+	bool ruleIsAlreadyInState(struct state currentState, string ruleInQuestion)
+	{
+		for (int i = 0; i < currentState.items.size(); i++)
+		{
+			if (ruleInQuestion.compare(currentState.items.at(i)) == 0) // If compare returns 0, the strings match
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Swaps the position of the dot with the next symbol in the item
+	string advanceDot(string s)
+	{
+		int pos = s.find('.');
+		if (pos != s.size() - 1)
+		{
+			swap(s[pos], s[pos +1]);
+		}
+		return s;
+	}
